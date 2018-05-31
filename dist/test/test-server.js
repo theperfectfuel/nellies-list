edadf27af45c66b643cc09411b644922c3e542bf
theperfectfuel <robert@theperfectfuel.com>
2018-05-31 11:01:16 -0700

    adding travis-ci setup

--- dist/test/test-server.js
+++ dist/test/test-server.js
@@ -1,33 +1,29 @@
 'use strict';
 
-var _chai = require('chai');
+var chai = require('chai');
+var chaiHttp = require('chai-http');
 
-var _chai2 = _interopRequireDefault(_chai);
+var _require = require('../index'),
+    app = _require.app,
+    runServer = _require.runServer,
+    closeServer = _require.closeServer;
 
-var _chaiHttp = require('chai-http');
+var expect = chai.expect;
 
-var _chaiHttp2 = _interopRequireDefault(_chaiHttp);
-
-var _index = require('../index');
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-var expect = _chai2.default.expect;
-
-_chai2.default.use(_chaiHttp2.default);
+chai.use(chaiHttp);
 
 describe('Todo List', function () {
 
     before(function () {
-        return (0, _index.runServer)();
+        return runServer();
     });
 
     after(function () {
-        return (0, _index.closeServer)();
+        return closeServer();
     });
 
     it('should return a list of todos on GET', function () {
-        return _chai2.default.request(_index.app).get('/').then(function (res) {
+        return chai.request(app).get('/').then(function (res) {
             expect(res).to.have.status(200);
         });
     });
